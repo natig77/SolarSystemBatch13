5a89018866129560cbab4868d55c64e108629da2
natig77 <natiggurbanov@gmail.com>
2022-08-29 23:35:50 -0400

    Solar system ready to be deployed

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -2,6 +2,11 @@ package com.syntax;
 
 public class Main {
     public static void main(String[] args) {
+        SolarSystem inception=new SolarSystem();
+        inception.sun=1;
+        inception.moon=4;
+        inception.planets=2;
 
+        System.out.println("the basic structure is ready");
     }
 }
